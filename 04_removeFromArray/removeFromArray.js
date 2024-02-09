26c01f3cecbedcddacb4fb99967b58ac06affae8
ChrisM345 <chrism.12277@gmail.com>
2024-02-09 15:30:26 -0600

    Completed 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(anArray, ...valuesToRemove) {
+    return anArray.filter((val) => !valuesToRemove.includes(val));
 };
 
+removeFromArray([1, 2, 3, 4], 3)
 // Do not edit below this line
 module.exports = removeFromArray;
