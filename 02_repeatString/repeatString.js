4c0a7e59ef51c98652751df30afc5d4cbd9844d0
ChrisM345 <chrism.12277@gmail.com>
2024-02-09 14:57:55 -0600

    Completed 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(text, timesToRepeat) {
+    if (timesToRepeat < 0){return 'ERROR';}
+    let answer = '';
+    for (let i = 0; i < timesToRepeat; i++){
+        answer += text;
+    }
+    return answer;
 };
 
 // Do not edit below this line
