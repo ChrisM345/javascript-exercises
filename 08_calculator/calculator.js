cc507a64a28ff23d0c9209830e26511e7fd6dd56
ChrisM345 <chrism.12277@gmail.com>
2024-02-10 10:10:09 -0600

    Completed 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	return numbers.reduce((prevVal, currVal) => prevVal + currVal, 0);
 };
 
-const multiply = function() {
+const multiply = function(numbers) {
+  return numbers.reduce((prevVal, currVal) => prevVal * currVal, 1);
 
 };
 
-const power = function() {
-	
+const power = function(num, pow) {
+  return num ** pow;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num == 0) return 1;
+  if (num == 1) return 1;
+
+  ans = 1;
+  for (; num > 1; num--){
+    ans *= num;
+  }
+  return ans;
+
 };
 
 // Do not edit below this line
