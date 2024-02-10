b51cbf86cad8bad5e5004c85f1a80299a10199b2
ChrisM345 <chrism.12277@gmail.com>
2024-02-10 10:34:51 -0600

    Completed 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(nth_number) {
+    if (nth_number < 0) return "OOPS";
+    if (nth_number == 0) return 0;
+    nth_number = parseInt(nth_number);
+    first_number = 1;
+    second_number = 2;
+    if (nth_number == 1 || nth_number == 2) return 1;
+    nth_number -= 2;
+    current_number = 1;
+    while(nth_number != 0){
+        second_number = first_number;
+        first_number = current_number;
+        current_number = first_number + second_number;
+        nth_number--;
+    }
+
+    return current_number;
 
 };
 
