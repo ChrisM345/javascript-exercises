79823f32d010069792f0bf6e94ebc8fada2f36b5
ChrisM345 <chrism.12277@gmail.com>
2024-02-10 10:19:26 -0600

    Completed 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, ''); 
+    string = string.replace(/\s+/g, '');
+    string = string.toLowerCase();
+    reversed_string = string.split("").reverse().join("");
+    return string == reversed_string;
 };
 
 // Do not edit below this line
