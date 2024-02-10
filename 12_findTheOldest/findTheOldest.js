89198daea4cb76cb5f55985870729e2870f521a2
ChrisM345 <chrism.12277@gmail.com>
2024-02-10 12:10:42 -0600

    Completed 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    const oldestPerson = array.reduce((oldest, currPerson) =>{
+        oldestDeath = oldest.yearOfDeath;
+        if (!oldestDeath) oldestDeath = new Date().getFullYear();
+        oldestAge = oldestDeath - oldest.yearOfBirth;
 
+        currPersonBirth = currPerson.yearOfBirth;
+        currPersonDeath = currPerson.yearOfDeath;
+        if(!currPersonDeath) currPersonDeath = new Date().getFullYear();
+        currPersonAge = currPersonDeath - currPersonBirth;
+
+        return currPersonAge > oldestAge ? currPerson : oldest
+    })
+
+    return oldestPerson;
 };
 
 // Do not edit below this line
